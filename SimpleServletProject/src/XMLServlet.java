8a10c2d9ed2bee9b127585ace89ae993ed408bc7
Evgeny Zelenin <transingularity@gmail.com>
2016-08-12 13:01:07 +0300

    Added multiple choose for <select> and out in String array.

--- SimpleServletProject/src/XMLServlet.java
+++ SimpleServletProject/src/XMLServlet.java
@@ -18,7 +18,16 @@ public class XMLServlet extends HttpServlet {
 		out.println("<h3>We know your full name: " + fullName + "</h3>");
 		
 		String prof = request.getParameter("prof");
-		out.println("You are a " + prof);
+		out.println("<p>You are a " + prof + "</p>");
+		
+	//	String location = request.getParameter("location");
+		String[] location = request.getParameterValues("location");
+		out.println("<p>You are at " + location.length + "places</p>");
+		
+		for(int i = 0; i < location.length; i++) {
+			out.println(location[i]);
+		}
+		
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
