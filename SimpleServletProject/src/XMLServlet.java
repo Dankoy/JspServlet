79fe047eb6d934a2ef053830f343a2b6edefa009
Evgeny Zelenin <transingularity@gmail.com>
2016-08-12 12:32:41 +0300

    Added new parameters and RadioButtons in form for request in servlet. doPost() method.

--- SimpleServletProject/src/XMLServlet.java
+++ SimpleServletProject/src/XMLServlet.java
@@ -20,7 +20,12 @@ public class XMLServlet extends HttpServlet {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		String userName = request.getParameter("userName");
-		out.println("Hello from doPost() method, " + userName);
+		String fullName = request.getParameter("fullName");
+		out.println("<h2>Hello from doPost() method, " + userName + "!</h2>");
+		out.println("<h3>We know your full name: " + fullName + "</h3>");
+		
+		String prof = request.getParameter("prof");
+		out.println("You are a " + prof);
 	}
 	
 }
