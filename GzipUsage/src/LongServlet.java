f12b119bbd88069f55b87f15023b750a1c61fbaa
Evgeny Zelenin <transingularity@gmail.com>
2016-08-18 09:48:29 +0300

    Added servlet that check for encoding header and print very long page

---
+++ GzipUsage/src/LongServlet.java
@@ -0,0 +1,63 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LongServlet
+ */
+@WebServlet(description = "Prints very long page")
+public class LongServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		
+		String docType = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 5.0 Transitional//EN\">\n";
+		String title = null;
+		
+		PrintWriter out;
+		
+		/* Check if gzip is supported and user didn't disable for particular request
+		 * If true then create gzipped PrinterWriter
+		 * If false then use regular PrintWriter 
+		 */
+		
+		if (Gzip.isGzipSupported(request) && !Gzip.isGzipDisabled(request)) {
+			out = Gzip.getGzipWriter(response);
+			response.setHeader("Content-Encoding", "gzip");
+			title = "Using Gzip Encoding";
+		}
+		else {
+			out = response.getWriter();
+			title = "Without Gzip Encoding";
+		}
+		
+		out.println(docType + 
+					"<html>\n" + 
+					"<head><title>" + title + "</title></head>\n" + 
+					"<body>\n" + 
+					"<h2 align = \"center\">" + title + "</h2>\n");
+		String line = "Let it be random words";
+		
+		for (int i = 0; i < 1000; i++) {
+			out.println(line);
+		}
+		
+		out.println("</body></html>");
+		out.close();
+
+	}
+
+}
